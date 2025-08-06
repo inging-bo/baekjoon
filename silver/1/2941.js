f49402d1806b3beaa97abb7a42b914e958367c39
HwangInBo <thervv@kakao.com>
2025-08-06 16:52:06 +0900

    풀이

---
+++ silver/1/2941.js
@@ -0,0 +1,55 @@
+const fs = require("fs");
+const input = fs.readFileSync("input.txt").toString().trim().split("\r\n");
+
+// const fs = require("fs");
+// const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const str = input[0].split("")
+console.log(str)
+let count = 0
+for (let i = 0; i < str.length; i++) {
+
+  if (i !== str.length - 1 && str[i] === "c") {
+    if (str[i + 1] === "=") {
+      count++
+      i++
+    } else if (str[i + 1] === "-") {
+      count++
+      i++
+    } else {
+      count++
+    }
+  } else if (i !== str.length - 1 && (str[i] === "l" || str[i] === "n")) {
+    if (str[i + 1] === "j") {
+      count++
+      i++
+    } else {
+      count++
+    }
+  } else if (i !== str.length - 1 && (str[i] === "s" || str[i] === "z")) {
+    if (str[i + 1] === "=") {
+      count++
+      i++
+    } else {
+      count++
+    }
+  } else if (i !== str.length - 1 && str[i] === "d") {
+    if (str[i + 1] === "-") {
+      count++
+      i++
+    } else {
+      count++
+    }
+  } else if (i !== str.length - 2 && str[i] === "d") {
+    if (str[i + 1] === "z" && str[i + 2] === "=") {
+      count++
+      i += 2
+    } else {
+      count++
+    }
+  } else {
+    count++
+  }
+}
+
+console.log(count)
