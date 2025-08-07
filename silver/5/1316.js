b2f6c978f1bd039c465e68e608fe9623d6d610ea
HwangInBo <thervv@kakao.com>
2025-08-07 13:11:56 +0900

    풀이

---
+++ silver/5/1316.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs.readFileSync("input.txt").toString().trim().split("\r\n");
+
+// const fs = require("fs");
+// const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const N = Number(input[0])
+for (let i = 1; i <= input.length; i++) {
+  
+
+
+}
--- silver/5/2941.js
+++ silver/5/2941.js
@@ -5,7 +5,6 @@ const input = fs.readFileSync("input.txt").toString().trim().split("\r\n");
 // const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 const str = input[0].split("")
-console.log(str)
 let count = 0
 for (let i = 0; i < str.length; i++) {
 
@@ -33,17 +32,13 @@ for (let i = 0; i < str.length; i++) {
     } else {
       count++
     }
-  } else if (i !== str.length - 1 && str[i] === "d") {
-    if (str[i + 1] === "-") {
-      count++
-      i++
-    } else {
-      count++
-    }
-  } else if (i !== str.length - 2 && str[i] === "d") {
+  } else if (str[i] === "d") {
     if (str[i + 1] === "z" && str[i + 2] === "=") {
       count++
       i += 2
+    } else if (str[i + 1] === "-") {
+      count++
+      i++
     } else {
       count++
     }
