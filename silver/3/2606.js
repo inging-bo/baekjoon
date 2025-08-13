7939a9fa86011fce7e989a8e72efa60d18099ad3
inging-bo <thervv@kakao.com>
2025-08-13 23:18:35 +0900

    풀이

--- silver/3/2606.js
+++ silver/3/2606.js
@@ -4,26 +4,53 @@ const input = fs.readFileSync("input.txt").toString().trim().split("\r\n");
 // const fs = require("fs");
 // const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
+
+//인접 리스트형태로 변환하면 탐색이 쉬워진다.
+
 const computers = Number(input[0])
 const twins = Number(input[1])
-let virus = 0
-let obj = {
-  "0" : input[2].split(" ").map(Number),
+
+let graph = Array.from({length : computers + 1}, () => [])
+console.log(graph)
+
+for (let i = 2; i < 2 + twins; i++) {
+  let [a, b] = input[i].split(" ").map(Number);
+  graph[a].push(b);
+  graph[b].push(a); // 무방향 그래프니까 양쪽 다 연결
 }
-let idx = 0
-for (let i = 0; i < twins - 1; i++) {
-  let [one, two] = input[3 + i].split(" ").map(Number)
-  console.log(i)
-  for (let [key, value] of Object.entries(obj) ) {
-    if (value.includes(one)) {
-      value.push(two)
-    } else if (value.includes(two)) {
-      value.push(one)
-    } else {
-      obj[++idx] = [one, two]
+
+let visited = Array(computers + 1).fill(false);
+let virus = [];
+
+function dfs(node) {
+  visited[node] = true;
+  for (let next of graph[node]) {
+    if (!visited[next]) {
+      virus.push(next);
+      dfs(next)
     }
   }
 }
-console.log(obj)
+
+dfs(1)
+
+function bfs(start) {
+  let queue = [start];
+  visited[start] = true;
+  
+  while (queue.length) {
+    let node = queue.shift();
+    for (let next of graph[node]) {
+      if (!visited[next]) {
+        visited[next] = true;
+        virus.push(next);
+        queue.push(next);
+      }
+    }
+  }
+}
+
+bfs(1);
+console.log(virus);
 
 
