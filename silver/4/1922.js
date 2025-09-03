6ce4d703dbdf09e2464f428d17fc0b2b4da9a649
inging-bo <thervv@kakao.com>
2025-09-04 00:43:40 +0900

    풀이

--- silver/4/1922.js
+++ silver/4/1922.js
@@ -7,27 +7,34 @@ const input = fs.readFileSync("input.txt").toString().trim().split("\r\n");
 const N = +input[0]
 const M = +input[1]
 let ans = Array.from({length : N + 1}, () => Array.from({length : N + 1}, () => 0))
-let visited = Array.from({length : N + 1}, () => Array.from({length : N + 1}, () => 0))
-let node = Array.from({length : N + 1}, () => [])
-console.log(visited)
+let visited = Array.from({length : N + 1}, () => 0 )
+let weight = Array.from({length : N + 1}, () => [])
 
 for (let i = 2; i < M + 2; i++) {
   const [a, b, c] = input[i].split(" ").map(Number)
   ans[a][b] = c
   ans[b][a] = c
-  node[a].push(b)
-  node[b].push(a)
+  weight[a].push(b)
+  weight[b].push(a)
 }
 
 function deep(a, b) {
-  console.log(ans[a][b])
+  console.log(a, b)
+  
+  
 }
 
+let find = 1
+let queue = []
+queue.push([1,0])
+console.log(queue)
 for (let i = 1; i < N; i++) {
-  for (let j = 0; j < node[i].length; j++) {
-    deep(i, node[i][j])
+  while (queue) {
+    let [a, b] = queue.shift()
+    weight[a][0], ans[a][weight[a][0]]
+    
   }
 }
-console.log(ans)
-console.log(node)
 
+console.log(ans)
+console.log(weight)
