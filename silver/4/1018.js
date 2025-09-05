66dd978b28064e60f4f442b7501d1acd291c46b2
inging-bo <thervv@kakao.com>
2025-09-05 20:59:08 +0900

    풀이

--- silver/4/1018.js
+++ silver/4/1018.js
@@ -7,34 +7,34 @@ const input = fs.readFileSync("input.txt").toString().trim().split("\r\n");
 const [N, K] = input[0].split(" ").map(Number)
 
 
-let ans = 64
-let min = 64
-let diffrent = []
 let counts = []
+let black = "BWBWBWBW"
+let white = "WBWBWBWB"
 for (let i = 1; i <= N - 7; i++) {
   for (let j = 0; j < K - 7; j++) {
-    let count = 0
+    let blackCount = 0
+    let whiteCount = 0
     for (let ii = i; ii < 8 + i; ii++) {
-      let slice = input[ii].slice(j, 8 + j).split("")
-      let one = ""
-      let two = ""
-      if (slice[0] === 'B') {
-        one = 'B'
-        two = 'W'
-      } else {
-        one = 'W'
-        two = 'B'
+      let k = 0
+      for (let jj = j; jj < 8 + j; jj++) {
+        if (input[ii][jj] !== black[k]) {
+          blackCount++
+        }
+        if (input[ii][jj] !== white[k]) {
+          whiteCount++
+        }
+        k++
       }
-      let even = slice.filter((item,i) => i % 2 === 0 && item !== one)
-      let odd = slice.filter((item,i) => i % 2 !== 0 && item !== two)
-      console.log(even, odd)
-      count += even.length + odd.length
-      // console.log(count)
+      let temp = black
+      black = white
+      white = temp
     }
-    counts.push(count)
-    console.log('------------------------')
+    counts.push(blackCount, whiteCount)
   }
 }
-// console.log(counts)
+console.log(Math.min(...counts))
+
+
+
 
 
