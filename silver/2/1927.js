7b0b75a0238ee088a7f468bdcb9aef732213f4cd
inging-bo <thervv@kakao.com>
2025-09-06 20:10:07 +0900

    풀이

--- silver/2/1927.js
+++ silver/2/1927.js
@@ -6,18 +6,37 @@ const input = fs.readFileSync("input.txt").toString().trim().split("\r\n");
 
 const N = +input[0];
 
-let queue = []
+let queue = [0]
 
-for (let i = 1; i <= N; i++) {
-  if (queue.length === 0 && !+input[i]) {
-    console.log('0')
-  } else if (!+input[i]) {
-
-  } else {
-    let j = +input[i]
-    while (j) {
-      queue
+function heap() {
+  for (let i = 1; i <= queue.length; i++) {
+    let temp
+    let left = queue[i * 2]
+    let right = queue[(i * 2) + 1]
+    console.log(left, right)
+    if (queue[i] > left) {
+      temp = left // 1
+      left = queue[i] // 12345678
+      queue[i] = temp
+      i *= 2
+      console.log(queue)
+    } else if (queue[i] > right) {
+      temp = right
+      right = queue[i]
+      queue[i] = temp
+      i = (i * 2) + 1
     }
   }
+  
 }
 
+for (let i = 1; i <= N; i++) {
+  if (queue.length === 1 && !+input[i]) {
+    console.log(queue[0],'hhh')
+  } else if (!+input[i]) {
+    console.log(queue)
+  } else {
+    queue.push(+input[i])
+    heap()
+  }
+}
