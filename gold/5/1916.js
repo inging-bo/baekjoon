7ee039e73270d9f74535fd790db80950b8dea981
inging-bo <thervv@kakao.com>
2025-09-17 23:09:04 +0900

    풀이

--- gold/5/1916.js
+++ gold/5/1916.js
@@ -14,11 +14,51 @@ for (let i = 2; i < input.length - 1; i++) {
   let [u, v, w] = input[i].split(" ").map(Number)
   node[u].push([v, w])
 }
-console.log(node)
 let dist = Array(N + 1).fill(Infinity)
 dist[start] = 0
 let heap = []
+heap.push([0, start])
 function pushHeap(item) {
-
+  heap.push(item)
+  let index = heap.length - 1
+  while (index > 0) {
+    let parent = Math.floor((index - 1) / 2)
+    if (heap[parent][0] > heap[index][0]) {
+      [heap[parent], heap[index]] = [heap[index], heap[parent]]
+    } else break
+    index = parent
+  }
 }
+
+function pushPop() {
+  if (heap.length === 1) return heap.pop()
+  let root = heap[0]
+  heap[0] = heap.pop()
+  let index = 0
+  while (true) {
+    let left = (2 * index) + 1;
+    let right = (2 * index) + 2;
+    let min = index;
+    if (left < heap.length && heap[left][0] < heap[min][0]) min = left;
+    if (right < heap.length && heap[right][0] < heap[min][0]) min = right;
+    if (min === index) break;
+    [heap[min] , heap[index]] = [heap[index] , heap[min]];
+    index = min
+  }
+  
+  return root
+}
+while (heap.length !== 0) {
+  let [curDist, cur] = pushPop()
+  if (curDist > dist[cur]) continue
+  for (let [next, weight] of node[cur]) {
+    if (dist[next] > curDist + weight) {
+      dist[next] = curDist + weight
+      pushHeap([dist[next], next])
+    }
+  }
+}
+console.log(dist[arrive])
+
+
 
