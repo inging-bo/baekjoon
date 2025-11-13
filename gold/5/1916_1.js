15a5f62b909a159ea9f31498d3250728509fcf0a
HwangInBo <174303095+inging-bo@users.noreply.github.com>
2025-11-13 17:29:57 +0900

    Update 1916_1.js

--- gold/5/1916_1.js
+++ gold/5/1916_1.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 const input = fs.readFileSync("input.txt").toString().trim().split("\r\n");
 
-// const fs = require("fs");
-// const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 const N = +input[0];
 const M = +input[1];
