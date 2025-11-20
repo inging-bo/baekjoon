42c2abad73b934de0b604c75e797a725a9369015
HwangInBo <174303095+inging-bo@users.noreply.github.com>
2025-11-20 13:05:26 +0900

    풀이

--- silver/5/7568.js
+++ silver/5/7568.js
@@ -6,20 +6,44 @@ const input = fs.readFileSync("input.txt").toString().trim().split("\r\n");
 
 const N = +input[0]
 
+let temp = []
+let ranks = {}
+let rank = 1
+for (let i = 1; i <= N; i++) {
+  temp.push(input[i].split(" ").map(Number))
+  ranks[temp[i - 1]] = 1
+}
+
+let sort = temp.sort(([a, c], [b, d]) => b - a)
+
+for (let i = 0; i < sort.length; i++) {
+  if (i !== sort.length - 1) {
+    let [curKg, curCm] = sort[i]
+    let [nextKg, nextCm] = sort[i + 1]
+
+    if (curKg > nextKg && curCm > nextCm) {
+      ranks[[curKg, curCm]] = rank++
+    } else if (curKg >= nextKg || curCm >= nextCm) {
+      ranks[[curKg, curCm]] = rank
+    } else {
+      ranks[[curKg, curCm]] = i + 1
+    }
+  } else {
+    let [prevKg, prevCm] = sort[i - 1]
+    let [curKg, curCm] = sort[i]
+    if (prevKg > curKg && prevCm > curCm) {
+      ranks[[curKg, curCm]] = i + 1
+    } else if (prevKg >= curKg || prevCm >= curCm) {
+      ranks[[curKg, curCm]] = rank
+    } else {
+      ranks[[curKg, curCm]] = i + 1
+    }
+  }
+}
 let answer = []
-
-for (let i = 1; i <= N; i++) {
-  answer.push(input[i].split(" ").map(Number))
+for (let [key, value] of Object.entries(ranks)) {
+  answer.push(value)
 }
+console.log(answer.join(" "))
 
-console.log(answer)
 
-let kg = []
-let cm = []
-let kg_cm = {}
-for (let i = 1; i <= N; i++) {
-  const [x, y] = input[i].split(" ").map(Number)
-  kg.push(x)
-  cm.push(y)
-}
-console.log(kg.sort((a,b) => b - a), cm.sort((a,b) => b - a))
