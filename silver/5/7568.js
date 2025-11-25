e5c8751349ffbddfbd1d225a4c2d4c39af6af613
inging-bo <thervv@kakao.com>
2025-11-25 22:03:03 +0900

    풀이

--- silver/5/7568.js
+++ silver/5/7568.js
@@ -6,44 +6,66 @@ const input = fs.readFileSync("input.txt").toString().trim().split("\r\n");
 
 const N = +input[0]
 
-let temp = []
-let ranks = {}
-let rank = 1
+let temps = []
+let ranks = Array.from({length : N}, () => 1)
+
 for (let i = 1; i <= N; i++) {
-  temp.push(input[i].split(" ").map(Number))
-  ranks[temp[i - 1]] = 1
+  temps.push(input[i].split(" ").map(Number))
 }
 
-let sort = temp.sort(([a, c], [b, d]) => b - a)
+for (let i = 0; i < N; i++) {
+  valueChk(i)
+}
 
-for (let i = 0; i < sort.length; i++) {
-  if (i !== sort.length - 1) {
-    let [curKg, curCm] = sort[i]
-    let [nextKg, nextCm] = sort[i + 1]
+function valueChk(num) {
+  for (let i = 0; i < N; i++) {
+    if (i !== num) {
+      let [chkKg, chkCm] = temps[num]
+      let [curKg, curCm] = temps[i]
 
-    if (curKg > nextKg && curCm > nextCm) {
-      ranks[[curKg, curCm]] = rank++
-    } else if (curKg >= nextKg || curCm >= nextCm) {
-      ranks[[curKg, curCm]] = rank
-    } else {
-      ranks[[curKg, curCm]] = i + 1
-    }
-  } else {
-    let [prevKg, prevCm] = sort[i - 1]
-    let [curKg, curCm] = sort[i]
-    if (prevKg > curKg && prevCm > curCm) {
-      ranks[[curKg, curCm]] = i + 1
-    } else if (prevKg >= curKg || prevCm >= curCm) {
-      ranks[[curKg, curCm]] = rank
-    } else {
-      ranks[[curKg, curCm]] = i + 1
+      if (chkKg > curKg && chkCm > curCm) {
+        ranks[i]++
+      }
     }
   }
 }
-let answer = []
-for (let [key, value] of Object.entries(ranks)) {
-  answer.push(value)
-}
-console.log(answer.join(" "))
+
+console.log(ranks.join(" "))
+// for (let i = 1; i <= N; i++) {
+//   temp.push(input[i].split(" ").map(Number))
+// }
+//
+// let sort = temp.sort(([a, c], [b, d]) => b - a)
+//
+//
+// for (let i = 0; i < sort.length; i++) {
+//   if (i !== sort.length - 1) {
+//     let [curKg, curCm] = sort[i]
+//     let [nextKg, nextCm] = sort[i + 1]
+//     if (curKg > nextKg && curCm > nextCm) {
+//       ranks[[curKg, curCm]] = rank++
+//     } else if (curKg >= nextKg || curCm >= nextCm) {
+//       ranks[[curKg, curCm]] = rank
+//     } else {
+//       ranks[[curKg, curCm]] = i + 1
+//     }
+//   } else {
+//
+//     let [prevKg, prevCm] = sort[i - 1]
+//     let [curKg, curCm] = sort[i]
+//     if (prevKg > curKg && prevCm > curCm) {
+//       ranks[[curKg, curCm]] = i + 1
+//     } else if (prevKg >= curKg || prevCm >= curCm) {
+//       ranks[[curKg, curCm]] = rank
+//     } else {
+//       ranks[[curKg, curCm]] = i + 1
+//     }
+//   }
+// }
+// let answer = []
+// for (let [key, value] of Object.entries(ranks)) {
+//   answer.push(value)
+// }
+// console.log(answer.join(" "))
 
 
