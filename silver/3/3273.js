469f99f24b8f7ac4459d98749b0ff5dc91411cd5
inging-bo <174303095+inging-bo@users.noreply.github.com>
2025-12-11 13:04:16 +0900

    파일전달

--- silver/3/3273.js
+++ silver/3/3273.js
@@ -7,5 +7,13 @@ const input = fs.readFileSync("input.txt").toString().trim().split("\r\n");
 const n = +input[0]
 const number = input[1].split(" ").map(Number)
 const x = +input[2]
-
-
+let answer = 0
+for (let i = 0; i <= n; i++) {
+  for (let j = i; j <= n; j++) {
+    if (i === j) continue
+    if (number[i] + number[j] === x) {
+      answer++
+    }
+  }
+}
+console.log(answer)
