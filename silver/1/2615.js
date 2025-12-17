0dabaf5931de052712edb78f65827975b6ec5f15
inging-bo <174303095+inging-bo@users.noreply.github.com>
2025-12-17 13:01:24 +0900

    풀이

--- silver/1/2615.js
+++ silver/1/2615.js
@@ -11,38 +11,21 @@ for (let i = 0; i < input.length; i++) {
 
 for (let i = 0; i < 19; i++) {
   for (let j = 0; j < 19; j++) {
-    if (i >= 19 || j >= 19) continue
     if (playBoard[i][j] === 1) {
-      search(i,j,1)
-    } else if (playBoard[i][j] === 2) {
-      search(i,j,2)
+      let [x, y] = [i, j]
+      let k = 0
+      let count = 0
+      const left = x - 1
+      const right = x + 1
+      const leftBottom = x - 1, y - 1
+      const bottom = y + 1
+      const rightBottom = x + 1, y + 1
+      while(k < 5) {
+
+        k++
+      }
     }
+
   }
 }
 
-function search(x,y, color) {
-  
-  if (x - 1 < 0 || y - 1 < 0 || x + 1 >= 19 || y + 1 >= 19 ) return
-  
-  let leftTop = playBoard[x - 1][y - 1]
-  let top = playBoard[x][y - 1]
-  let rightTop = playBoard[x + 1][y - 1]
-  let right = playBoard[x - 1][y]
-  let rightBottom = playBoard[x + 1][y + 1]
-  let bottom = playBoard[x][y + 1]
-  let leftBottom = playBoard[x - 1][y + 1]
-  let left = playBoard[x - 1][y]
-  const position = [leftTop, top, rightTop, right, rightBottom, bottom, leftBottom, left]
-  for (let i = 0; i < 8; i++) {
-    if (position[i] === color) {
-      console.log(position[i])
-    }
-  }
-  
-  
-  let count = 0
-  let i = 0
-  while (i !== 0) {
-  
-  }
-}
